001363fb98d6a9427e260c66b39df8e7bd89f970
firefoxmmx2 <firefoxmmx2>
2013-08-27 23:11:36 +0800

    添加用户控制器

---
+++ app/controllers/User.scala
@@ -0,0 +1,14 @@
+package controllers
+
+import play.api._
+import play.api.mvc._
+import play.api.Play.current
+import play.api.data._
+import views.html.defaultpages.todo
+object User {
+  def login = todo
+  def isLogin = todo
+  def add = todo
+  def update = todo
+  def delete = todo
+}
